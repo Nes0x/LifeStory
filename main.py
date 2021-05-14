bc69d39f9ee6fb9fdda33099d8eee7af4cd551de
Nes0x <66802265+Nes0x@users.noreply.github.com>
2021-05-14 19:07:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import random
 
 def game():
-    name = ["Andrzej", "Kuba", "Janusz", "Adrian", "Artur", "Stasiek", "Marek", "Sławek", "Maciek", "Mirosław", "Karol", "Patryk", "Zuzia", "Aleksandra", "Ola", "Nina"]
-    rich = ["Bogaty/a", "Biedny/a"]
-    deadInformation = ["apokalipsy", "wypadku samochodowego", "katastrofy lotniczej", "zestarzenia", "powód niezany"]
-    profession = ["Programista", "Budowlaniec", "Lekarz", "Kasjer", "Nauczyciel", "Architekt", "Pracownik obsługi klienta"]
-    birthday = ["Styczniu", "Lutym", "Marcu", "Kwietniu", "Maju", "Czerwcu", "Lipcu", "Sierpniu", "Wrześniu", "Październiku", "Listopadzie", "Grudniu"]
+    name = ("Andrzej", "Kuba", "Janusz", "Adrian", "Artur", "Stasiek", "Marek", "Sławek", "Maciek", "Mirosław", "Karol", "Patryk", "Zuzia", "Aleksandra", "Ola", "Nina")
+    rich = ("Bogaty/a", "Biedny/a")
+    deadInformation = ("apokalipsy", "wypadku samochodowego", "katastrofy lotniczej", "zestarzenia", "powód niezany")
+    profession = ("Programista", "Budowlaniec", "Lekarz", "Kasjer", "Nauczyciel", "Architekt", "Pracownik obsługi klienta")
+    birthday = ("Styczniu", "Lutym", "Marcu", "Kwietniu", "Maju", "Czerwcu", "Lipcu", "Sierpniu", "Wrześniu", "Październiku", "Listopadzie", "Grudniu")
 
 
-
-    valueOfNames = len(name) - 1
-    valueOfDeadInformation = len(deadInformation) - 1
-    valueOfProfession = len(profession) - 1
-    valueOfBirthday = len(birthday) - 1
-
-    birthdayRand = random.randint(0, valueOfBirthday)
-    professionRand = random.randint(0, valueOfProfession)
-    nameRand = random.randint(0, valueOfNames)
-    richRand = random.randint(0, 1)
+    birthdayRand = random.choice(birthday)
+    professionRand = random.choice(profession)
+    nameRand = random.choice(name)
+    richRand = random.choice(rich)
     age = random.randint(16, 100)
     dead = random.randint(age, 100)
-    deadInformationRand = random.randint(0, valueOfDeadInformation)
+    deadInformationRand = random.choice(deadInformation)
     marry = random.randint(age, dead)
 
     if richRand == 0:
@@ -28,12 +22,18 @@ def game():
     else:
         money = random.randint(2800, 4000)
 
-    print(f"Masz na imie {name[nameRand]} \nMasz {age} lat/a \nJesteś {rich[richRand]} \nUrodziłeś/aś się w {birthday[birthdayRand]} \nSwoją drugą połówkę znalazłeś/aś w wieku {marry} lat \nMiesięcznie zarabiasz {money} zł \nPracujesz jako {profession[professionRand]} \nUmarłes/aś w wieku {dead} lat w wyniku {deadInformation[deadInformationRand]}")
-
+    print(f"""Masz na imie {nameRand} 
+Masz {age} lat/a
+Jesteś {richRand}
+Urodziłeś/aś się w {birthdayRand}
+Swoją drugą połówkę znalazłeś/aś w wieku {marry} lat
+Miesięcznie zarabiasz {money} zł
+Pracujesz jako {professionRand}
+Umarłes/aś w wieku {dead} lat w wyniku {deadInformationRand}""")
 
 while True:
-    gameInformation = input("Czy chcesz grać? Wpisz nie jeśli chcesz przerwać, jeśli chcesz grać dalej kliknij enter")
-    if gameInformation == "nie":
+    gameInformation = input("Czy chcesz grać? Wpisz 1 jeśli chcesz przerwać, jeśli chcesz grać dalej kliknij enter")
+    if gameInformation == "1":
         break
     else:
         game()
